3c173664b788fdf66457d425b59c14d4c2b35e24
Rudy Casilla <rpccode@gmail.com>
2022-01-16 16:53:09 -0500

    Primeros pasos de este Proyecto

---
+++ js/app.js
@@ -0,0 +1,31 @@
+import *  as UI from "./interfaz.js";
+
+
+UI.formulariobuscar.addEventListener('submit', buscarCancion);
+
+
+
+function buscarCancion(e){
+    e.preventDefault();
+
+
+
+    const artista = document.querySelector('#artista').value;
+    const cancion = document.querySelector('#cancion').value;
+
+    if (artista === ''|| cancion === '') {
+
+        UI.DivMensaje.textContent = 'Error... Todos los campos son Obligatorios';
+        UI.DivMensaje.classList.add('error');
+
+        setTimeout(() => {
+                UI.DivMensaje.textContent = '';
+                UI.DivMensaje.classList.remove('error');
+        }, 3000);
+    }
+
+    ///Consultar  Api
+        
+
+
+}
---
+++ js/interfaz.js
@@ -0,0 +1,5 @@
+export const formulariobuscar =  document.querySelector('#formulario-buscar'),
+                     DivBuscar = document.querySelector('#buscar'),
+                     DivMensaje = document.querySelector('#mensajes'),
+                     DiivResultado = document.querySelector('#resultado'),
+                     HedingResultado = document.querySelector('#letra-resultado');
